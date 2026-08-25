2d54464f37bfe6fe8d1d9799d53c7b8ce1cd7c05
Purnendu Achary <purnenduachary95@gmail.com>
2026-08-25 23:58:53 +0530

    Track client source code

---
+++ client/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import {ModeToggle} from "@/components/ui/mode-toggle";
+
+export default function Home() {
+  return (
+   <div>
+     <ModeToggle />
+   </div>
+  );
+}
